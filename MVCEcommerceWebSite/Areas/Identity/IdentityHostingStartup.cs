1d241b7c41ebc7e5df2ed747af4c5437d99adcb1
alaahamdy20 <alaahamdy201295@gmail.com>
2022-02-25 18:05:07 +0200

    Add Identity & Layout

---
+++ MVCEcommerceWebSite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MVCEcommerceWebSite.Data;
+
+[assembly: HostingStartup(typeof(MVCEcommerceWebSite.Areas.Identity.IdentityHostingStartup))]
+namespace MVCEcommerceWebSite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
